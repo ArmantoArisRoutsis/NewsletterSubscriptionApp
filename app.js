9e052c22084a3788f2a3055e58173f70c74bd448
Aris Armanto Routsis <67515158+ArmantoArisRoutsis@users.noreply.github.com>
2020-10-08 22:55:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+//https://sheltered-dusk-26973.herokuapp.com/
+const express = require("express")
+const request = require("request")
+const bodyParser = require("body-parser")
+const https = require("https")
+
+const app = express()
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/failure.html", function(req,res){
+    res.redirect("/")
+})
+
+app.post("/", function(req,res){
+    const firstName = req.body.fname
+    const lastName = req.body.lname
+    const email = req.body.email
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merged_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data)
+
+    const url = "https://us2.api.mailchimp.com/3.0/lists/804c2c71fa"
+
+    const options = {
+        method: "POST",
+        auth: "ArisR:a948a8562414ce382795a271a08197c1-us2"
+    }
+    const request = https.request(url, options, function(response){
+        if (response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html")
+        }else{
+            res.sendFile(__dirname+"/failure.html")
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data))
+        })
+    });
+
+    request.write(jsonData)
+    request.end()
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("The server is running on port 3000:")
+});
+
+//api
+//a948a8562414ce382795a271a08197c1-us2
+
